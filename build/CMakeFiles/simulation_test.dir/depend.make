af03fc477a7264c7825f4c3372e47d4e380c1d39
lmilz <milzlars88@gmail.com>
2025-12-27 20:57:26 +0100

    delete build folder

--- build/CMakeFiles/simulation_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for simulation_test.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lmilz/Second-Brain/Projects/random-organization
-# Build directory: /home/lmilz/Second-Brain/Projects/random-organization/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/lmilz/Second-Brain/Projects/random-organization/build/simulation_test[1]_include.cmake")
